9d5fff7ff8c481c9a9d3ddaeeec6937e3b7f6b0b
ellielle <40385743+ellielle@users.noreply.github.com>
2023-10-24 12:47:17 -0400

    already miss expanding directories

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -132,17 +132,17 @@ require("lazy").setup({
       })
     end
   },
-  --  {
-  --    "nvim-tree/nvim-tree.lua",
-  --    version = "*",
-  --    lazy = false,
-  --    dependencies = {
-  --      "nvim-tree/nvim-web-devicons"
-  --    },
-  --    config = function()
-  --      require("nvim-tree").setup {}
-  --    end
-  --  },
+  {
+    "nvim-tree/nvim-tree.lua",
+    version = "*",
+    lazy = false,
+    dependencies = {
+      "nvim-tree/nvim-web-devicons"
+    },
+    config = function()
+      require("nvim-tree").setup {}
+    end
+  },
   --  {
   --    "nvim-neo-tree/neo-tree.nvim",
   --    branch = "v3.x",
