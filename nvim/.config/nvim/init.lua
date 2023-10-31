0a5b385bbf45c3eb4d6affb0942b745c5e0c587c
ellielle <40385743+ellielle@users.noreply.github.com>
2023-10-31 13:19:08 -0400

    fix formatting on TS projects and add a format key for svelte

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -2,19 +2,18 @@
 require("core.options")
 local keymaps = require("core.keymaps")
 
-keymaps.init()              -- initialize and load general keymaps
-keymaps.plugins_keymaps()   -- load plugin-based keymaps
+keymaps.init() -- initialize and load general keymaps
+keymaps.plugins_keymaps() -- load plugin-based keymaps
 
-require("core.lazy")        -- load lazy.nvim package manager
+require("core.lazy") -- load lazy.nvim package manager
 require("core.colorscheme") -- set colorscheme
 
 -- fix errors not clearing in some cases with TS
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
-  underline = true,
-  virtual_text = {
-    spacing = 5,
-    severity_limit = "Warning",
-  },
-  update_in_insert = true,
+	underline = true,
+	virtual_text = {
+		spacing = 5,
+		severity_limit = "Warning",
+	},
+	update_in_insert = true,
 })
-
